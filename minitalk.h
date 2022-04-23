acba298367b5e7e0bd5c63d2db4e8aaa56be07ea
Abdessalam Mounach <amounach@e1r11p4.1337.ma>
2022-04-23 02:12:43 +0000

    fix: fix norminett

--- minitalk.h
+++ minitalk.h
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 02:08:41 by amounach          #+#    #+#             */
+/*   Updated: 2022/04/23 02:08:59 by amounach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
 
-struct utils
+struct s_utils
 {
 	int	i;
 	int	pid;
-}t_utils;
+};
 
 void	ft_putnbr(unsigned int nb);
 int		ft_atoi(const char *str);
